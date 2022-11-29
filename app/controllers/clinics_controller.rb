b7b1683e3a442896f50ef05cc4998082e98bac55
Aran Seehra <aranseehra@gmail.com>
2022-11-29 15:25:08 +0000

    add clinic controller and view page

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,2 +1,22 @@
 class ClinicsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
+  def index
+    @clinics = Clinic.all
+  end
+
+  private
+
+  def new
+    @clinic = Clinic.new
+  end
+
+  def create
+    @clinic = Clinic.find(clinic_params)
+    @clinic.save
+  end
+
+  def clinic_params
+    params.require(:clinic).permit(:exam_type, :body_part, :address, :price)
+  end
 end
