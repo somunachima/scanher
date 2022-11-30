78cae58997e3b0dad8cb9200f77b769bf11ee90e
Som Akudu <somakudu@gmail.com>
2022-11-30 15:40:19 +0000

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
 
-  resources :clinics, only: [:index, :new, :create ] do
-    resources :exams
+  resources :clinics, only: [ :index, :new, :create ] do
+    resources :exams, only: [ :new, :create ]
+
+  resources :bookings, except: [ :edit, :update ]
+
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
