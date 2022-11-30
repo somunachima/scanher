9aee99a6708c5548e22c953e40622d770ec1a3da
Som Akudu <somakudu@gmail.com>
2022-11-30 16:20:32 +0000

    update routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :dashboard
+  def dashboard
+    @user = current_user
+  end
+
+  def photo
+
+  end
+end
