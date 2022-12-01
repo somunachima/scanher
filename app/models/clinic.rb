302ca62b5e20a97ec1364cd5bcda20d0f989b38b
Ariel Lima <ariel_lima_oliveira@yahoo.com.br>
2022-12-01 16:56:53 +0000

    geocoder and index layout done

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,4 +1,6 @@
 class Clinic < ApplicationRecord
   has_many :exams, dependent: :destroy
   has_many :timeslots, through: :exams
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_172135) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_130113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_172135) do
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "exams", force: :cascade do |t|
