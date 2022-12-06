48c92383f2fc83a382978f4fd682ebc4f23fedf4
Som Akudu <somakudu@gmail.com>
2022-12-06 02:18:47 +0000

    Clean up dashboard and homepage. Fix update user edit form

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -4,6 +4,6 @@ class Exam < ApplicationRecord
 
   SERVICE = ["MRI", "CT", "Ultrasound Scan", "Pregnancy"]
   # To call on - Exam::SERVICE
-  BODY_PART = ["Breast", "Lumps & Bumps", "Ovarian", "Armpit/Axilla", "Hernia", "Pregnancy Scan", "Brain/Head", "Neck & Thyroid", "Transvaginal", "Leg", "Hip", "Hip - Bilateral", "Arm", "Knee", "Lower Spine", "Upper Spine", "Whole Spine", "Pelvis", "Full Body Scan", "Cardiac (Heart)", "Shoulder", "Chest", "Ankle", "Wrist", "Elbow", "Soft Tissue (Muscular/Ligamental", "Foot (Morton's Neuroma)", "Abdominal", "Facial Bones", "Fracture Assessment", "Pelvic", "Kidney & Renal", "Musculoskeletal (MSK)", "Vascular & Arterial", "Testicular"]
+  BODY_PART = ["Breast", "Lumps & Bumps", "Ovarian", "Armpit/Axilla", "Hernia", "Pregnancy Scan", "Brain/Head", "Neck & Thyroid", "Transvaginal", "Leg", "Hip", "Hip - Bilateral", "Arm", "Knee", "Lower Spine", "Upper Spine", "Whole Spine", "Pelvis", "Full Body Scan", "Cardiac (Heart)", "Shoulder", "Chest", "Ankle", "Wrist", "Elbow", "Soft Tissue (Muscular/Ligamental)", "Foot (Morton's Neuroma)", "Abdominal", "Facial Bones", "Fracture Assessment", "Pelvic", "Kidney & Renal", "Musculoskeletal (MSK)", "Vascular & Arterial", "Testicular"]
   # To call on - Exam::BODY_PART
 end
