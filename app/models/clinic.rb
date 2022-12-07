a705e401208953251b9eafd624895805fa858ec8
Som Akudu <somakudu@gmail.com>
2022-12-07 15:43:00 +0000

    Fixed seeding

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,5 @@
 class Clinic < ApplicationRecord
   has_many :exams, dependent: :destroy
-  has_many :chatrooms, dependent: :destroy
   has_many :timeslots, through: :exams
 
   has_many :bookings, through: :timeslots
